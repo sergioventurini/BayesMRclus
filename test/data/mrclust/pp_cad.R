e63a623d0b6cbb41bb4eb5c30e5f97b744c77627
sergioventurini <sergio.venturini@gmail.com>
2026-05-28 09:23:35 +0200

    changes
    changes

---
+++ test/data/mrclust/pp_cad.R
@@ -0,0 +1,58 @@
+library(TwoSampleMR)
+library(RadialMR)
+library(BayesMRclus)
+
+data("PP_CAD", package = "mrclust")
+pp_cad <- PP_CAD[, 2:6]
+colnames(pp_cad) <- c("SNP", "beta.exposure", "se.exposure", "beta.outcome", "se.outcome")
+save(pp_cad, file = "/Users/Sergio/dev/BayesMRclus/data/pp_cad.rda")
+
+# dat_orig <- pp_cad
+# 
+# beta_wald <- dat_orig$beta.outcome/dat_orig$beta.exposure
+# se_beta_wald <- abs(dat_orig$se.outcome/dat_orig$beta.exposure)
+# hist(beta_wald, breaks = 20)
+# I2(beta_wald, se_beta_wald)
+# 
+# radial <- format_radial(dat_orig$beta.exposure,
+#                         dat_orig$beta.outcome,
+#                         dat_orig$se.exposure,
+#                         dat_orig$se.outcome,
+#                         dat_orig$SNP)
+# ivw.object <- ivw_radial(radial, 0.05, 1, 0.0001, FALSE)
+# egg.object <- egger_radial(radial, 0.05, 1, FALSE)
+# plot_radial(ivw.object)
+# plot_radial(egg.object)
+# 
+# sd(beta_wald) > median(sqrt((dat_orig$se.outcome^2)/(dat_orig$beta.exposure^2)))
+
+# z_exp <- abs(dat_orig$beta.exposure/dat_orig$se.exposure)
+# plot(z_exp, beta_wald, pch = 20,
+#      col="darkblue",
+#      xlab="Instrument strength |Z_gamma|",
+#      ylab="Wald ratio estimate")
+# abline(h = 0, lty = 2)
+# # add IVW estimate
+# ivw <- mr(dat_orig, method_list = "mr_ivw")
+# abline(h = ivw$b, col = "red", lwd = 2)
+# # smooth trend
+# lines(lowess(z_exp, beta_wald), col = "black", lwd = 2)
+# precision <- 1/dat_orig$se.outcome
+# symbols(z_exp, beta_wald,
+#         circles = precision/max(precision),
+#         inches = 0.1,
+#         fg = "darkblue",
+#         add = TRUE)
+
+# # devtools::install_github("cnfoley/mrclust")
+# library(ggplot2)
+# library(dplyr)
+# library(mrclust)
+# 
+# sel <- dat_orig$beta.exposure < 0
+# dat_orig$beta.exposure[sel] <- -dat_orig$beta.exposure[sel]
+# dat_orig$beta.outcome[sel] <- -dat_orig$beta.outcome[sel]
+# 
+# mrclust_res <- mrclust_results(dat_orig)
+# mrclust_res$p1
+# mrclust_res$tab
---
+++ test/examples_het.R
@@ -0,0 +1,74 @@
+library(BayesMRclus)
+
+# prepare data
+data("bmi_sbp", package = "BayesMRclus")
+bmi_sbp <- subset(bmi_sbp, pval_selection < 5e-4)
+
+n <- nrow(bmi_sbp)
+zhaodata <- new("bayesmr_data", data = bmi_sbp, reorientation = TRUE)
+data_tmp <- shaodata@data
+# summary(zhaodata)
+# plot(zhaodata, se = TRUE)
+
+# simulation setup
+# note: psi and tau proposal standard deviations are on log scale (see paper)
+prm.prop <- list(beta = .4, psi = .15, tau = .15)
+burnin <- 100000
+nsim <- 200000
+
+nchains <- 3
+# seed <- 2301
+control <- list(burnin = burnin, nsim = nsim, beta.prop = prm.prop[["beta"]],
+                psi.prop = prm.prop[["psi"]], tau.prop = prm.prop[["tau"]],
+                seed = NULL, random_start = TRUE, verbose = TRUE,
+                nchains = nchains, thin = 50,
+                store.burnin = TRUE, threads = ifelse(
+                  nchains <= parallel::detectCores() - 1,
+                  nchains, parallel::detectCores() - 1),
+                parallel = "snow")
+
+alpha_psi <- 0.05 #alpha_eb(se = data_tmp[, "se_exposure"])
+alpha_tau <- 2*alpha_psi #alpha_eb(se = data_tmp[, "se_outcome"])
+prior <- bayesmr_prior(psi = list(alpha = alpha_psi, nu = 3),
+                       tau = list(alpha = alpha_tau, nu = 3),
+                       gamma = list(mean = 0, var = 1e2),
+                       beta = list(mean = 0, var = 1e2))
+
+# MCMC simulation
+system.time(res_BMR <- bayesmr_het(zhaodata, control = control, prior = prior))
+
+# MCMC summaries/graphs
+summary(res_BMR)
+plot(res_BMR, what = "trace", regex_pars = "psi")
+plot(res_BMR, what = "trace", regex_pars = "tau")
+plot(res_BMR, what = "trace", regex_pars = "beta")
+plot(res_BMR, what = "trace", regex_pars = "gamma")
+# plot(res_BMR, what = "hist_by_chain", regex_pars = "tau")
+# plot(res_BMR, what = "dens_chains", regex_pars = "beta")
+# plot(res_BMR, what = "intervals", regex_pars = "beta")
+# plot(res_BMR, what = "acf_bar", regex_pars = "tau")
+
+# coda analyses
+library(coda)
+# res_BMR_sub <- subset(res_BMR, regex_pars = c("gamma", "beta"))
+res_BMR_sub <- subset(res_BMR, regex_pars = c("psi", "tau"))
+plot(res_BMR_sub)
+cumuplot(res_BMR_sub)
+gelman.plot(res_BMR_sub)
+geweke.plot(res_BMR_sub)
+raftery.diag(res_BMR_sub)
+HPDinterval(res_BMR_sub)
+heidel.diag(res_BMR_sub)
+densplot(res_BMR_sub)
+autocorr.diag(res_BMR_sub)
+effectiveSize(res_BMR_sub)
+
+###
+
+# inverse-variance weighted method
+library(MendelianRandomization)
+
+mr_ivw(mr_input(data$beta_exposure, data$se_exposure,
+                data$beta_outcome, data$se_outcome))
+mr_allmethods(mr_input(data$beta_exposure, data$se_exposure,
+                       data$beta_outcome, data$se_outcome))
